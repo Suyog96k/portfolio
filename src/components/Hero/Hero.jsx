da2d8334cde6c351245ba287b1b7d67de434c15e
Suyog Chavan <109721278+Suyog96k@users.noreply.github.com>
2025-03-19 17:32:04 +0530

    Add files via upload

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import styles from './Hero.module.css';
+import { getImageUrl } from '../../utils';
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Suyog Chavan</h1>
+        <p className={styles.description}>
+          I'm a frontend developer with 1.2 years of experience using
+          React and React Native. Reach out if you'd like to learn
+          more!
+        </p>
+        <a
+          href="mailto:suyogchavan96k@gmail.com.com"
+          className={styles.contactBtn}
+        >
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl('hero/heroo.png')}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
